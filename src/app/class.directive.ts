e6d8835314e12b6ed1cd0a3cebe32c780c10a7b8
LebokgangSteve <lebokgang@digilink.africa>
2023-05-09 10:15:13 +0200

    commit

---
+++ src/app/class.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appClass]',
+})
+export class ClassDirective {
+  //@Input() backgroundColor: string = '';
+  constructor(private element: ElementRef) {
+    //Never do still
+    // setTimeout(() => {
+    //   this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    // }, 50);
+  }
+  @Input() set backgroundColor(color: string) {
+    this.element.nativeElement.style.backgroundColor = color;
+  }
+}
