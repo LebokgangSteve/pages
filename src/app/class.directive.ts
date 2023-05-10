4c3c82f99bbdfe5c63b7e8b14d81e18006d7460d
LebokgangSteve <lebokgang@digilink.africa>
2023-05-10 13:55:44 +0200

    commit

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -11,7 +11,14 @@ export class ClassDirective {
     //   this.element.nativeElement.style.backgroundColor = this.backgroundColor;
     // }, 50);
   }
-  @Input() set backgroundColor(color: string) {
-    this.element.nativeElement.style.backgroundColor = color;
+  @Input('appClass') set classNames(classObj: any) {
+   for(let key in classObj){
+    if(classObj[key]){
+    this.element.nativeElement.classList.add(key);
+   }else{
+    this.element.nativeElement.classList.remove(key)
+   }
+  
   }
+}
 }
